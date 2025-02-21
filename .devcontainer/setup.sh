0942e8eb4df407bf06906bea77b7e1ed4beeac75
Will Yaeger <108743050+wyaeger@users.noreply.github.com>
2025-02-21 16:16:19 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,115 +16,64 @@ sudo apt install -y rename zip unzip curl wget nano build-essential pkg-config a
 
 # Download datasets
 cd /workspaces/biodim-workshop/data
-wget http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionA.zip
-wget http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionB.zip
-wget http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionC.zip
-wget http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionD.zip
-wget http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionE.zip
+wget -q --show-progress http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionA.zip
+wget -q --show-progress http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionB.zip
+wget -q --show-progress http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionC.zip
+wget -q --show-progress http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionD.zip
+wget -q --show-progress http://learnmetabarcoding.github.io/LearnMetabarcoding/data/sectionE.zip
 
 # Unzip datasets
-unzip sectionA.zip -d sectionA
-unzip sectionB.zip -d sectionB
-unzip sectionC.zip -d sectionC
-unzip sectionD.zip -d sectionD
-unzip sectionE.zip -d sectionE
+unzip -o sectionA.zip -d sectionA
+unzip -o sectionB.zip -d sectionB
+unzip -o sectionC.zip -d sectionC
+unzip -o sectionD.zip -d sectionD
+unzip -o sectionE.zip -d sectionE
 rm sectionA.zip sectionB.zip sectionC.zip sectionD.zip sectionE.zip
 
-# Install BLAST
-BLAST_VERSION="2.11.0"
-cd /usr/local/src/
-sudo wget "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${BLAST_VERSION}/ncbi-blast-${BLAST_VERSION}+-x64-linux.tar.gz"
-sudo tar -zxf ncbi-blast-${BLAST_VERSION}+-x64-linux.tar.gz
-sudo cp ncbi-blast-${BLAST_VERSION}+/bin/* /usr/local/bin/
+# Ensure FastQC is installed
+sudo apt install -y fastqc
 
-# Install bPTP
-cd /usr/local/src
-sudo git clone https://github.com/zhangjiajie/PTP
-cd PTP
-sudo -H python3 -m pip install -r requirements.txt
-cd bin
-for f in *.py; do sed -i "1s/python$/python3/" $f; done
-sudo ln -s /usr/local/src/PTP/bin/bPTP.py /usr/local/bin/bPTP
-sudo ln -s /usr/local/src/PTP/bin/PTP.py /usr/local/bin/PTP
-
-# Install catfasta2phyml
-sudo wget -O /usr/local/bin/catfasta2phyml.pl https://raw.githubusercontent.com/nylander/catfasta2phyml/master/catfasta2phyml.pl
-sudo chmod a+x /usr/local/bin/catfasta2phyml.pl
+# Install VSEARCH
+sudo apt install -y vsearch
 
 # Install Cutadapt
-sudo -H python3 -m pip install cutadapt
-
-# Install extract_genes
-sudo apt install -y bioperl libarray-utils-perl
-sudo wget -O /usr/local/bin/extract_genes.pl https://raw.githubusercontent.com/tjcreedy/biotools/master/extract_genes.pl
-sudo chmod a+x /usr/local/bin/extract_genes.pl
-
-# Install FastTree
-cd /usr/local/bin/
-sudo wget http://www.microbesonline.org/fasttree/FastTree
-sudo wget http://www.microbesonline.org/fasttree/FastTreeMP
-sudo chmod a+x FastTree*
-
-# Install FastQC
-cd /usr/local/src/
-sudo wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip
-sudo unzip fastqc_v0.11.9.zip
-sudo chmod a+x FastQC/fastqc
-sudo ln -s /usr/local/src/FastQC/fastqc /usr/local/bin/fastqc
-
-# Install FASTX Toolkit
-cd /usr/local/src/
-sudo wget https://github.com/agordon/libgtextutils/releases/download/0.7/libgtextutils-0.7.tar.gz
-sudo tar -xzf libgtextutils-0.7.tar.gz
-cd libgtextutils-0.7
-sudo ./configure
-sudo make
-sudo make install
-cd ../
-sudo wget https://github.com/agordon/fastx_toolkit/releases/download/0.0.14/fastx_toolkit-0.0.14.tar.bz2
-sudo tar -xjf fastx_toolkit-0.0.14.tar.bz2
-cd fastx_toolkit-0.0.14
-sudo ./configure
-sudo make
-sudo make install
-cd ../
+sudo -H python3 -m pip install --upgrade cutadapt
 
 # Install MAFFT
-sudo wget -P /usr/local/deb/ https://mafft.cbrc.jp/alignment/software/mafft_7.475-1_amd64.deb
-sudo apt install -y /usr/local/deb/mafft_7.475-1_amd64.deb
+sudo apt install -y mafft
 
-# Install metaMATE
-sudo -H python3 -m pip install metaMATE
-sudo Rscript -e "install.packages(c('getopt', 'ape', 'fastcluster'), repos = 'https://cloud.r-project.org')"
+# Install FastTree
+sudo apt install -y fasttree
 
 # Install Pairfq
+sudo apt install -y libdbd-sqlite3-perl
 sudo curl -sL cpanmin.us | sudo perl - git://github.com/sestaton/Pairfq.git
 
-# Install phylostuff
-cd /usr/local/src
-sudo git clone https://github.com/tjcreedy/phylostuff.git
-cd phylostuff
-sudo ./install.sh
-
-# Install swarm
+# Install Swarm
 cd /usr/local/src
 sudo git clone https://github.com/torognes/swarm.git
 cd swarm/
 sudo make
 sudo cp bin/swarm /usr/local/bin/
 
-# Install VSEARCH
-cd /usr/local/src
-sudo wget https://github.com/torognes/vsearch/archive/v2.15.1.tar.gz
-sudo tar xzf v2.15.1.tar.gz
-cd vsearch-2.15.1
-sudo ./autogen.sh
-sudo ./configure
-sudo make
-sudo make install
+# Install metaMATE
+sudo -H python3 -m pip install --upgrade metaMATE
+sudo Rscript -e "install.packages(c('getopt', 'ape', 'fastcluster'), repos = 'https://cloud.r-project.org')"
+
+# Verify installations
+fastqc --version
+blastn -version
+vsearch --version
+cutadapt --version
+mafft --version
+fasttree -help
+pairfq --version
+swarm --version
+metaMATE --version
 
 # Cleanup
 sudo apt autoremove -y
 sudo apt clean
 
 echo "Setup complete!"
+
